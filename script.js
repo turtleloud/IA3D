d0d31f9ffa0887c8d4a5a4d50090a1e408e011a3
turtleloud <146945920+turtleloud@users.noreply.github.com>
2024-10-10 08:35:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,71 +2,81 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPergunta = document.querySelector('.caixa-pergunta');
 const caixaAlternativa = document.querySelector('.caixa-alternativa');
 const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
         enunciado: "Você gosta da ideia de Inteligência Artificial?",
-        alternativas:[
+        alternativas: [
             {
-            texto:"Sim",
-            afirmativa:"Afirmativa da alternativa 1"
-        },
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
             {
-            texto: "Não",
-            afirmativa:"Afirmativa da Alternativa 2"
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
             }
         ]
     },
     {
         enunciado: "Pergunta 2",
-        alternativas:[{
-            texto:"Sim",
-            afirmativa:"Afirmativa da alternativa 1"
-        },
+        alternativas: [
             {
-            texto: "Não",
-            afirmativa:"Afirmativa da Alternativa 2"
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
+            {
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
             }
         ]
     },
     {
         enunciado: "Pergunta 3",
-        alternativas:[
+        alternativas: [
             {
-            texto:"Sim",
-            afirmativa:"Afirmativa da alternativa 1"
-        },
+                texto: "Sim",
+                afirmativa: "Afirmativa da alternativa 1"
+            },
             {
-            texto: "Não",
-            afirmativa:"Afirmativa da Alternativa 2"
+                texto: "Não",
+                afirmativa: "Afirmativa da alternativa 2"
             }
         ]
-    } 
-]  
-
+    }
+]
 
 let posicao = 0;
 let perguntaAtual;
 let respostas = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (posicao >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
+    caixaAlternativa.textContent = " ";
     mostraAlternativas();
 }
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativa){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa)
+        botaoAlternativas.addEventListener("click",() => respostaSelecionada(alternativa));
         caixaAlternativa.appendChild(botaoAlternativas);
     }
 }
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmativa;
-    respostas = afirmacoes;
+    respostas += afirmacoes;
     posicao++;
     mostraPergunta();
-
 }
-mostraPergunta();
+function mostraResultado(){
+    caixaPergunta.textContent = "Em 25 anos...";
+    textoResultado.textContent = respostas;
+    
+}
+mostraPergunta()
